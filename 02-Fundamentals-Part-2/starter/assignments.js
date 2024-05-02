3ae56c6a222539879392e814e700c8fccdda822b
Nacho <ignacio.nahuel.ramos@gmail.com>
2024-05-02 17:18:38 -0300

    first commit

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,17 @@
+// // -------- Values and Variables--------
+
+// let country = "Argentina";
+// let continent = "America";
+// let population = 44000000;
+
+// console.log(country, continent, population)
+
+// // -------- Data types --------
+
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland)
+// console.log(typeof population)
+// console.log(typeof country)
+// console.log(typeof language)
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,24 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+*/
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Nacho");
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null)
